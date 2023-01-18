6e0778cbf7b30ee9b5a2ea1a416c9447e71b5a71
Jaeyoung Lim <limjyjustin@gmail.com>
2023-01-18 10:54:54 +0900

    Add files via upload

---
+++ src/main/java/dev/limjustin/deployprac/DeployPracApplication.java
@@ -0,0 +1,13 @@
+package dev.limjustin.deployprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeployPracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeployPracApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/limjustin/deployprac/DeployPracApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.limjustin.deployprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeployPracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
