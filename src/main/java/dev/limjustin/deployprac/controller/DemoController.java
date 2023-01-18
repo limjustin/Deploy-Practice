31898022c871163eac41e6e03448eaec3b9ee89f
limjustin <limjyjustin@gmail.com>
2023-01-18 13:31:34 +0900

    Test Deploy

---
+++ src/main/java/dev/limjustin/deployprac/controller/DemoController.java
@@ -0,0 +1,18 @@
+package dev.limjustin.deployprac.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    // Github Action에서 주입받은 값으로 변경됨을 확인하기 위한 변수
+    @Value("${override.value}")
+    private String overrideValue;
+
+    @GetMapping("/demo")
+    public String demo() {
+        return overrideValue;
+    }
+}
